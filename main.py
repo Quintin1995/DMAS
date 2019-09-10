6f4621fc71a376d69aef09595328d80f3972918a
C. Roest <fake@fake.com>
2019-09-10 21:06:16 +0200

    integer agent en secret namen omdat het alfabet maar 26 letters heeft

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ class LiarType (Enum):
     BLUFFER     = 1
     SABOTEUR    = 2
 
-AMOUNT_AGENTS   = 3
-MAX_SECRET      = 2
+AMOUNT_AGENTS   = 22
+MAX_SECRET      = 22
 TRANSFER_CHANCE = 100
 PROTOCOL        = Protocols.ANY
-ITERATIONS      = 3
+ITERATIONS      = 60
 possible_secrets = list(range(MAX_SECRET))
 
 def get_secret_value (agent_idx, target_idx):
@@ -32,10 +32,10 @@ def agent_has_information (agent_idx, target_idx):
     return False
 
 def get_agent_codename (agent_idx): 
-    return "Agent {}".format("ABCDEFGHIJKLMNOPQRSTUVWXYZ"[agent_idx])
+    return "Agent {}".format(agent_idx + 1)
 
 def get_secret_codename (secret_idx):
-    return "secret {}".format("abcdefghijklmnopqrstuvwxyz"[secret_idx])
+    return "secret {}".format(secret_idx + 1)
 
 def get_agent_secret (agent_idx):
     return [idx for idx, s in enumerate(secrets[agent_idx][agent_idx]) if s == max(secrets[agent_idx][agent_idx])][0]
@@ -81,7 +81,7 @@ for agent_idx in range (AMOUNT_AGENTS):
 
 # Main loop
 for iteration in range(ITERATIONS):
-    print (secrets)
+    # print (secrets)
     if PROTOCOL == Protocols.ANY:
         caller, receiver = choose_callers_any(agents, calls)
     
@@ -94,7 +94,7 @@ for iteration in range(ITERATIONS):
 
 for target_idx in range (AMOUNT_AGENTS):
     for agent_idx in range (AMOUNT_AGENTS):
-        print ("{} thinks {} has secret {}".format(get_agent_codename(agent_idx), get_agent_codename(target_idx), get_secret_codename(get_secret_value(agent_idx, target_idx))))
+        print ("{} thinks {} has {}".format(get_agent_codename(agent_idx), get_agent_codename(target_idx), get_secret_codename(get_secret_value(agent_idx, target_idx))))
 
     agent_secret = get_agent_secret(target_idx)
     print ("{} has {}".format(get_agent_codename(target_idx), get_secret_codename(agent_secret)))
@@ -102,8 +102,8 @@ for target_idx in range (AMOUNT_AGENTS):
 
 # for i in ITERATIONS: 
 #     print (""l)
-print (calls)
-print (avg_knowledge)
-print ("--------------------------")
+# print (calls)
+# print (avg_knowledge)
+# print ("--------------------------")
 
 print ()
