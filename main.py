2a30806af679fa10006702c3604e57617e384bc1
C. Roest <fake@fake.com>
2019-09-10 20:59:58 +0200

    Werkende integer secrets

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from enum import Enum
-from tqdm import tqdm
 import math
 import random
 
@@ -18,42 +17,55 @@ AMOUNT_AGENTS   = 3
 MAX_SECRET      = 2
 TRANSFER_CHANCE = 100
 PROTOCOL        = Protocols.ANY
-ITERATIONS      = 5
+ITERATIONS      = 3
 possible_secrets = list(range(MAX_SECRET))
 
-def get_secret_value (agent_idx, secret_idx):
-    agent_knowledge = agents[agent_idx]["secrets"][secret_idx]
+def get_secret_value (agent_idx, target_idx):
+    agent_knowledge = secrets[agent_idx][target_idx]
     highest_secrets = [idx for idx, s in enumerate(agent_knowledge) if s == max(agent_knowledge)]
     return random.choice(highest_secrets)
 
-def agent_knows_secret (agent_idx, secret_idx):
-    agent_knowledge = agents[agent_idx]["secrets"][secret_idx]
+def agent_has_information (agent_idx, target_idx):
+    agent_knowledge = secrets[agent_idx][target_idx]
     if (max(agent_knowledge)) > 0:
         return True
     return False
 
+def get_agent_codename (agent_idx): 
+    return "Agent {}".format("ABCDEFGHIJKLMNOPQRSTUVWXYZ"[agent_idx])
+
+def get_secret_codename (secret_idx):
+    return "secret {}".format("abcdefghijklmnopqrstuvwxyz"[secret_idx])
+
+def get_agent_secret (agent_idx):
+    return [idx for idx, s in enumerate(secrets[agent_idx][agent_idx]) if s == max(secrets[agent_idx][agent_idx])][0]
+
 # Initialize agents
-agents = list ([dict () for a in range (AMOUNT_AGENTS)])
+agents  = list ([dict () for a in range (AMOUNT_AGENTS)])
+secrets = list ()
 
-for idx, a in enumerate(agents):
-    a["name"]           = idx
-    a["secrets"]        = list ()
-    for o in range(AMOUNT_AGENTS): 
-        a["secrets"].append(list())
-        for s in possible_secrets:
-            a["secrets"][o].append(0)
+for agent_idx in range (AMOUNT_AGENTS):
+    secrets.append(list())
+    for target_idx in range (AMOUNT_AGENTS): 
+        secrets[agent_idx].append(list())
+        for secret_idx in range (MAX_SECRET):
+            secrets[agent_idx][target_idx].append(list())
+            secrets[agent_idx][target_idx][secret_idx] = 0
+    
+    secrets[agent_idx][agent_idx][random.choice(possible_secrets)] = 999999
 
-    a["secrets"][idx][random.choice(possible_secrets)] = 999999
-    a["addressbook"]    = list()
 
 # Initialize call log
 calls = list ()
 avg_knowledge = list ()
 
-def transfer_secrets(caller, receiver):
-    for secret in range(AMOUNT_AGENTS): # Same amount of secrets as agents
-        if (agent_knows_secret(caller, secret)):
-            agents[receiver]["secrets"][secret][get_secret_value(caller, receiver)] += 1
+def transfer_secrets(sender, receiver):
+    for target_agent in range(AMOUNT_AGENTS): # Same amount of secrets as agents
+        # Agents know their own secret
+        if (target_agent == receiver):
+            continue
+        if (agent_has_information(sender, target_agent)):
+            secrets[receiver][target_agent][get_secret_value(sender, target_agent)] += 1
 
 # Define protocol
 def choose_callers_any(agents, calls):
@@ -63,8 +75,13 @@ def choose_callers_any(agents, calls):
         receiver = random.choice(range(AMOUNT_AGENTS))
     return (caller, receiver)
 
+for agent_idx in range (AMOUNT_AGENTS):
+    agent_secret = get_agent_secret(agent_idx)
+    print ("{} has {}".format(get_agent_codename(agent_idx), get_secret_codename(agent_secret)))
+
 # Main loop
-for iteration in tqdm(range(ITERATIONS)):
+for iteration in range(ITERATIONS):
+    print (secrets)
     if PROTOCOL == Protocols.ANY:
         caller, receiver = choose_callers_any(agents, calls)
     
@@ -74,11 +91,19 @@ for iteration in tqdm(range(ITERATIONS)):
     calls.append(tuple((caller, receiver)))
     # avg_knowledge.append (sum ([len([True for s in agents[a]["secrets"] if s == True]) for a in range (AMOUNT_AGENTS)]) / AMOUNT_AGENTS)
 
-    for a in agents:
-        print (a)
+
+for target_idx in range (AMOUNT_AGENTS):
+    for agent_idx in range (AMOUNT_AGENTS):
+        print ("{} thinks {} has secret {}".format(get_agent_codename(agent_idx), get_agent_codename(target_idx), get_secret_codename(get_secret_value(agent_idx, target_idx))))
+
+    agent_secret = get_agent_secret(target_idx)
+    print ("{} has {}".format(get_agent_codename(target_idx), get_secret_codename(agent_secret)))
+    print ()
 
 # for i in ITERATIONS: 
 #     print (""l)
 print (calls)
 print (avg_knowledge)
 print ("--------------------------")
+
+print ()
