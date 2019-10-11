38e32c132bf7160fa13b0c3fc45917f65657f989
Quintin van Lohuizen <qvlohuizen@live.nl>
2019-10-11 12:50:41 +0200

    added plots to ui when running experiments

--- Controller.py
+++ Controller.py
@@ -302,5 +302,50 @@ class Controller():
     def Btn_perform_experiments (self, event):
         amount_experiments = int(self.amount_experiments.get())
         max_iters = int(self.max_amount_iters_experiments.get())
+
         results = self.model.do_experiment(amount_experiments, max_iters)
 
+        for iteration, result in enumerate(results):
+            print("Trial {}: {}".format(iteration, result))
+
+        print (results)
+
+        plot_data = [v for (s, v) in results if s == 'DONE']
+
+        print (plot_data)
+        plot_data_mean = sum(plot_data) / float(len(plot_data))
+        plot_data_std  = np.std(plot_data)
+        # plt.hist(plot_data, bins=25, edgecolor='k', alpha=0.65)
+        # plt.ylabel('Occurences')
+        # plt.show()
+
+        fig, ax = plt.subplots()
+
+        # the histogram of the data
+        n, bins, patches = ax.hist(plot_data, 25, density=1, edgecolor='k', alpha=0.65)
+        plt.axvline(sum(plot_data)/float(len(plot_data)), color='k', linestyle='dashed', linewidth=1)
+
+        # add a 'best fit' line
+        y = ((1 / (np.sqrt(2 * np.pi) * plot_data_std)) *
+            np.exp(-0.5 * (1 / plot_data_std * (bins - plot_data_mean))**2))
+        ax.plot(bins, y, '--r')
+        ax.set_xlabel('Smarts')
+        # Tweak spacing to prevent clipping of ylabel
+        fig.tight_layout()
+        plt.show()
+
+        categories = ['Success', 'Failed', 'Did not finish']
+        values = list()
+        values.append(len(list([v for s, v in results if s == 'DONE'])))
+        values.append(len(list([v for s, v in results if s == 'NO_CALLS'])))
+        values.append(len(list([v for s, v in results if s == 'RUN'])))
+        fig, ax = plt.subplots()
+        bar = ax.bar(categories, values, alpha=0.4)
+        for rect in bar:
+            height = rect.get_height()
+            plt.text(rect.get_x() + rect.get_width()/2.0, height, '%10.2f%%' % (int(height)/float(len(results))), ha='center', va='bottom')
+        plt.show()
+
+        fig, ax = plt.subplots()
+        ax.boxplot(plot_data)
+        plt.show()
